3d3e1696f0885642874b884d66a67684b615ced5
Ahmed <s.ahmedma@gmail.com>
2024-10-07 23:03:05 +0500

    avitar added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,8 @@ import MainNav from '@/components/main-nav';
 import React from 'react';
 import { MdArrowBack} from 'react-icons/md';
 import { FiSettings } from 'react-icons/fi';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 export default function Home() {
 
@@ -17,7 +19,18 @@ export default function Home() {
     <MdArrowBack size={24} />
     Profile
     <FiSettings size={24} /> 
+    
+    </div>
+
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+
+      <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </div>
   </div>
+
+
   );
 }
