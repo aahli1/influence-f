9eee90347720f9d33c68bb7df5f075307411c7c6
Ahmed <s.ahmedma@gmail.com>
2024-10-07 22:59:04 +0500

    profile link is added, icons configured

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import MainNav from '@/components/main-nav';
+import React from 'react';
+import { MdArrowBack} from 'react-icons/md';
+import { FiSettings } from 'react-icons/fi';
+
+export default function Home() {
+
+
+  return (
+
+
+
+    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+
+    <MdArrowBack size={24} />
+    Profile
+    <FiSettings size={24} /> 
+    </div>
+  </div>
+  );
+}
