03a39505c7b9fd0df0d0cdf4997ccf28057e0448
Ahmed <s.ahmedma@gmail.com>
2024-10-08 13:05:39 +0500

    first four elements applied

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,32 +4,39 @@ import React from 'react';
 import { MdArrowBack} from 'react-icons/md';
 import { FiSettings } from 'react-icons/fi';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
+import './profile.css'; 
 
 export default function Home() {
 
 
   return (
 
+<div className="container">
 
+  <div className="row">
+    <div className="arrow">←</div>
+    <div className="profileText">Profile</div>
+    <div className="settings">    <FiSettings size={24} /> </div> 
+  </div>
 
-<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 ">
-
-    <MdArrowBack size={24} />
-    <div>Profile</div>
-      
-    <FiSettings size={24} /> 
-    
-    </div>
-
-
-      <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
+  <div className="avatarRow"> 
+    <Avatar>
+      <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
       </Avatar>
   </div>
 
+  <div className="row">
+    <div className="centerText">Item 1</div>
+        <div className="centerText">Item 2</div>
+        <div className="centerText">Item 3</div>
+  </div>
+
+  <div className="textboxRow">
+        <input type="text" className="textbox" placeholder="Enter text here" />
+  </div>
+
+</div>
 
   );
 }
